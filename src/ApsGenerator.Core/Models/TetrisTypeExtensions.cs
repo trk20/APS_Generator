adabaa8a2b35cfc79b99c03213352cdd1b6cdcc6
Timothy Kennedy <70790401+trk20@users.noreply.github.com>
2026-07-05 12:45:46 -0400

    Effective autoloader display and workflow update (#34)

---
+++ src/ApsGenerator.Core/Models/TetrisTypeExtensions.cs
@@ -0,0 +1,15 @@
+namespace ApsGenerator.Core.Models;
+
+public static class TetrisTypeExtensions
+{
+    public static int ClipCount(this TetrisType type) => type switch
+    {
+        TetrisType.ThreeClip => 3,
+        TetrisType.FourClip => 4,
+        TetrisType.FiveClip => 5,
+        _ => throw new ArgumentOutOfRangeException(nameof(type), type, "Unsupported tetris type.")
+    };
+
+    public static int EffectiveAutoloadersPerPlacement(this TetrisType type) =>
+        type.ClipCount() + 1;
+}
